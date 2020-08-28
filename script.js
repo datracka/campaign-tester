66219af21a62fd9cbd0f9a5a90b100b3b063df96
datracka <datracka@gmail.com>
2020-08-28 17:25:31 +0200

    add again lyticsID and get _ui

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let CONTAINER_ID = "form-container";
 let FIRST_NAME = "Vicens";
 let LAST_NAME = "Fayos";
 let EMAIL = "datracka@gmail.com";
-let PHONE = "(213) 341 343";
+let TELEPHONE = "(213) 341 343";
 
 //* End of custom configuration *//
 
@@ -31,28 +31,42 @@ function getUrlParam(parameter, defaultvalue) {
   return urlparameter;
 }
 
-let utmTerm = getUrlParam("utm_term", "");
-let utmSource = getUrlParam("utm_source", "");
-let utmContent = getUrlParam("utm_content", "");
-let utmMedium = getUrlParam("utm_medium", "");
+// when all scripts are loaded
+window.addEventListener(
+  "load",
+  () => {
+    try {
+      jstag.getid((id) => {
+        let numericPartOfUid = id.slice(2);
+        let utmTerm = getUrlParam("utm_term", "");
+        let utmSource = getUrlParam("utm_source", "");
+        let utmContent = getUrlParam("utm_content", "");
+        let utmMedium = getUrlParam("utm_medium", "");
 
-ENABLE_DEBUGGING
-  ? console.log(
-      "parameters to send to lytics",
-      `utm_term=${utmTerm} utm_source${utmSource} utm_content=${utmContent} utm_medium=${utmMedium}`
-    )
-  : null;
-// create iFrame and append to `div`
-let d = document.getElementById(CONTAINER_ID);
-let ifrm = document.createElement("iframe");
-ifrm.setAttribute(
-  "src",
-  `https://takeaction.citizenwindow.io/?uc=${UNIQUE_CAMPAIGN}&uf=${UNIQUE_FORM}&utm_term=${utmTerm}&utm_source=${utmSource}&utm_content=${utmContent}&utm_medium=${utmMedium}&first_name=${FIRST_NAME}&last_name=${LAST_NAME}&email=${EMAIL}&phone=${PHONE}`
+        ENABLE_DEBUGGING
+          ? console.log(
+              "parameters to send to lytics",
+              `utm_term=${utmTerm} utm_source${utmSource} utm_content=${utmContent} utm_medium=${utmMedium}`
+            )
+          : null;
+        // create iFrame and append to `div`
+        let d = document.getElementById(CONTAINER_ID);
+        let ifrm = document.createElement("iframe");
+        ifrm.setAttribute(
+          "src",
+          `https://takeaction.citizenwindow.io/?uc=${UNIQUE_CAMPAIGN}&uf=${UNIQUE_FORM}&_uid=${numericPartOfUid}&utm_term=${utmTerm}&utm_source=${utmSource}&utm_content=${utmContent}&utm_medium=${utmMedium}&first_name=${FIRST_NAME}&last_name=${LAST_NAME}&email=${EMAIL}&TELEPHONE=${TELEPHONE}`
+        );
+        ifrm.setAttribute("id", "advocates-iframe");
+        ifrm.setAttribute("width", "100%");
+        ifrm.setAttribute("height", "720px");
+        ifrm.setAttribute("allowtransparency", true);
+        ifrm.setAttribute("frameborder", "0");
+        ifrm.setAttribute("scrolling", "no");
+        d.appendChild(ifrm);
+      });
+    } catch (err) {
+      console.log("Lytics jstag not found!!", err);
+    }
+  },
+  false
 );
-ifrm.setAttribute("id", "advocates-iframe");
-ifrm.setAttribute("width", "100%");
-ifrm.setAttribute("height", "720px");
-ifrm.setAttribute("allowtransparency", true);
-ifrm.setAttribute("frameborder", "0");
-ifrm.setAttribute("scrolling", "no");
-d.appendChild(ifrm);
